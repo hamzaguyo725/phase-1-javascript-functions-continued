5505af13b0eaa49393f4528aeaf25781ce264850
hamzaguyo725 <hamzaguyo725@gmail.com>
2023-03-19 11:43:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun (activity) {
+  if(activity==null) {return "This Saturday, I want to roller-skate!"}
+  else {return `This Saturday, I want to `+ activity +`!`}
+}
+console.log(activity('bathe my dog'))
+
+function mondayWork(activity){
+    if (activity==null){return "This Monday, I will go to the office."}
+    else{return `This Monday, I will `+ activity +`.` }
+}
+console.log(activity='work from home')
+
+
+
+function wrapAdjective(name){
+    return function wrap(emphatic){
+        return `You are ${name}`+ emphatic + `${name}!`;
+    }
+    }
